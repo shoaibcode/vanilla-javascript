161e08e0cf46077a75b90006213100c961fc3d9d
shoaibbhimani <shoaibbhimani1392@gmail.com>
2018-11-18 03:34:48 +0530

    Create DOM and Add to UL

--- custom.js
+++ custom.js
@@ -1,1 +1,22 @@
-console.log("===");
+//Get Form tag
+var form = document.querySelector("#addTodo");
+
+var todo = document.querySelector("#todo");
+// Get UL Tag
+var ul = document.querySelector("ul");
+
+//add Event (Submit) to Form Tag
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // Create LI tag
+  var li = document.createElement("li");
+  li.setAttribute("class", "list-item");
+  // add new value to LI tag
+  li.innerHTML = todo.value;
+
+  // Add li tag to UL
+  ul.appendChild(li);
+
+  todo.value = "";
+});
